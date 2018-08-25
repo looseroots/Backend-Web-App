9f4b397e7ca487cf4a45793ea3f0ebf4f39f1be9
Cody Swain <swain.cody@gmail.com>
2018-08-25 15:06:20 -0700

    Upload user

--- app/views.py
+++ app/views.py
@@ -1,9 +1,27 @@
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
+
 from flask import redirect, url_for, render_template, request, jsonify
 from app.events import get_events
 from app import app, gmaps
 
+cred = credentials.Certificate('looseroots-7a373-firebase-adminsdk-wwa1y-60d6bc043b.json')
+default_app = firebase_admin.initialize_app(cred)
+
+# Get a database reference to server
+ref = db.reference('server')
+
 @app.route('/')
 def index():
+
+	users_ref = ref.child('users')
+
+	users_ref.set({
+	    'username': username
+	    'bio': bio
+	})
+
 	return jsonify(pingus='pongus')
 
 @app.route('/events')
