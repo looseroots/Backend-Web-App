649d93ba189ad93c351a87322c26286751c83057
Cody Swain <swain.cody@gmail.com>
2018-08-25 16:20:31 -0700

    Working firestore database

--- app/forms.py
+++ app/forms.py
@@ -4,4 +4,6 @@ from wtforms.validators import DataRequired
 
 class UserForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
+	firstname = StringField('First Name', validators=[DataRequired()])
+	lastname = StringField('Last Name', validators=[DataRequired()])
 	bio = StringField('Bio', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -3,25 +3,34 @@ from app.forms import UserForm
 from app import app, gmaps, cred
 
 import firebase_admin
-from firebase_admin import db
+from firebase_admin import firestore
 
-default_app = firebase_admin.initialize_app(cred)
+default_app = firebase_admin.initialize_app(cred, {'databaseURL': 'https://looseroots-7a373.firebaseio.com'})
+# ref = db.reference(path="/", app=default_app)
+
+db = firestore.client()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	userform = UserForm()
 	if userform.validate_on_submit():
 		username = userform.username.data
-		bio = userform.bio.data
 
-		users_ref = db.child('users')
+		data = {
+			u'firstname': str(userform.firstname.data),
+			u'lastname' : str(userform.lastname.data),
+			u'bio'		: str(userform.bio.data),
+		}
 
-		users_ref.set({
-		    'username': username,
-		    'bio': bio
-		})
+		# users_ref = ref.child('users')
+		user_id = db.collection(u'users').document(username).set(data)
 
-		return 'Successfully submitted form data with username {}, bio {}'.format(username, bio)
+		# users_ref.set({
+		#     'username': username,
+		#     'bio': bio
+		# })
+
+		return 'Successfully submitted form data with username'
 
 
 	return render_template("index.html", userform=userform)
