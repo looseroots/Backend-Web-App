a6e3d8b149990000f03d046fe9b20f7efc721af7
Cody Swain <swain.cody@gmail.com>
2018-08-25 15:37:32 -0700

    Copied from jack

--- app/views.py
+++ app/views.py
@@ -1,35 +1,27 @@
+from flask import redirect, url_for, render_template, request, jsonify
+from app.forms import UserForm
+from app import app, gmaps, cred
+
 import firebase_admin
 from firebase_admin import db
-from firebase_admin import credentials
 
-from flask import redirect, url_for, render_template, request, jsonify
-from app.events import get_events
-from app import app, gmaps
-
-cred = credentials.Certificate('looseroots-7a373-firebase-adminsdk-wwa1y-60d6bc043b.json')
 default_app = firebase_admin.initialize_app(cred)
 
-# Get a database reference to server
-ref = db.reference('server')
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+	userform = UserForm()
+	if userform.validate_on_submit():
+		username = userform.username.data
+		bio = userform.bio.data
 
-	users_ref = ref.child('users')
+		users_ref = db.child('users')
 
-	users_ref.set({
-	    'username': username
-	    'bio': bio
-	})
+		users_ref.set({
+		    'username': username,
+		    'bio': bio
+		})
 
-	return jsonify(pingus='pongus')
+		return 'Successfully submitted form data with username {}, bio {}'.format(username, bio)
 
-@app.route('/events')
-def events():
-	location = request.args.get('location', None, type=int)
 
-	return jsonify(eventlist=get_events(location))
-
-@app.route('/profile')
-def profile():
-	pass
+	return render_template("index.html", userform=userform)
--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 GMAPS_API_KEY='AIzaSyCNmgccUPpy_GChmm7b721Q0p6VzXyy3j4'
+
+SECRET_KEY='adsljfad;jsfkadsnfkasdkfgalsdhflshdfaln'
