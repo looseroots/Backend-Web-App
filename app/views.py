34fd05dcc21a1931ee8347cb34df2b6be1f857d3
jack <jackmnewsom@gmail.com>
2018-08-25 15:04:30 -0700

    Removed extra print

--- app/views.py
+++ app/views.py
@@ -8,8 +8,6 @@ def index():
 	if userform.validate_on_submit():
 		username = userform.username.data
 		bio = userform.bio.data
-		print(username, bio)
-
 		return 'Successfully submitted form data with username {}, bio {}'.format(username, bio)
 
 	return render_template("index.html", userform=userform)
