68b77d1d1ef3b50a04f9d08cb33027bd06b09487
Cody Swain <swain.cody@gmail.com>
2018-08-25 17:30:47 -0700

    Bootstrapped

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
-class UserForm(FlaskForm):
+class CreateUser(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
-	firstname = StringField('First Name', validators=[DataRequired()])
-	lastname = StringField('Last Name', validators=[DataRequired()])
-	bio = StringField('Bio', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired()])
+	password = StringField('Password', validators=[DataRequired()])
+	first_name = StringField('First Name', validators=[DataRequired()])
+	phone = StringField('Phone Number', validators=[DataRequired()])
+
+class EditProfile(FlaskForm):
+	pass
+	# bio = StringField('')
--- app/views.py
+++ app/views.py
@@ -1,36 +1,38 @@
 from flask import redirect, url_for, render_template, request, jsonify
-from app.forms import UserForm
+from app.forms import CreateUser, EditProfile
 from app import app, gmaps, cred
 
-import firebase_admin
+import firebase_admin #?
+from firebase_admin import auth
 from firebase_admin import firestore
 
 default_app = firebase_admin.initialize_app(cred, {'databaseURL': 'https://looseroots-7a373.firebaseio.com'})
-# ref = db.reference(path="/", app=default_app)
-
 db = firestore.client()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	userform = UserForm()
-	if userform.validate_on_submit():
-		username = userform.username.data
+	create_user_form = CreateUser()
+	# edit_profile_form = EditProfile()
 
-		data = {
-			u'firstname': str(userform.firstname.data),
-			u'lastname' : str(userform.lastname.data),
-			u'bio'		: str(userform.bio.data),
-		}
-
-		# users_ref = ref.child('users')
-		user_id = db.collection(u'users').document(username).set(data)
-
-		# users_ref.set({
-		#     'username': username,
-		#     'bio': bio
-		# })
-
-		return 'Successfully submitted form data with username'
+	if create_user_form.validate_on_submit():
+		user = auth.create_user(
+			uid=str(create_user_form.username.data),
+			email=str(create_user_form.email.data),
+			email_verified=True,
+			phone_number=str(create_user_form.phone.data),
+			password=str(create_user_form.password.data),
+			display_name=str(create_user_form.display_name.data),
+			disabled=False
+		)
+		print('Sucessfully created new user: {0}'.format(user.uid))
 
 
-	return render_template("index.html", userform=userform)
+	# if edit_profile_form.validate_on_submit():
+	# 	user_info = {
+	# 		u'bio': profileform.bio.data,
+	# 		u'profile_picture': profileform.profile_picture.data
+	# 	}
+	# 	db.collection(u'users').document(user.uid).set(user_info)
+
+
+	return render_template("index.html", userform=create_user_form)
